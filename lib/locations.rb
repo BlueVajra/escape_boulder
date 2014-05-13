a9a5938cf68e51ec4e7d3826db9121db3d7f67b7
BlueVajra <cory.leistikow@gmail.com>
2014-05-12 18:46:58 -0600

    add lcoations

---
+++ lib/locations.rb
@@ -0,0 +1,15 @@
+LOCATIONS =[
+  {
+    location: 0,
+    name: "9th and Pearl",
+    description: "A beautiful spot at the west"
+  },
+{
+  location: 1,
+  name: "10th and Pearl",
+  description: "A beautiful spot at the west"
+}
+  
+
+
+]
