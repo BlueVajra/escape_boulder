4bfa00dd5a5c495a8c75cdb5fb6b0160d3bd0221
marthaberner <marthaberner.mb@gmail.com>
2014-05-12 20:18:15 -0600

    Finished Items list

--- lib/items.rb
+++ lib/items.rb
@@ -10,10 +10,25 @@ ITEMS =
 
     "Ozo" => ["old guy in biker shorts", "milf", "lesbians"],
 
-    "Pasta Jays" => ["asshole looking for gluten-free pasta", "fat CU Alumni reliving the glory days", "expensive, crap food"],
+    "Pasta Jay's" => ["asshole looking for gluten-free pasta", "fat CU Alumni reliving the glory days", "expensive, crap food"],
 
     "Hapa Sushi" => ["a room ergonomically designed to reduce paranoia", "blue dream", "threesome"],
 
-    "Boulder Book Store" => ["mystical author", "cure for evil", "man who just ran 100 miles" ]
+    "Boulder Book Store" => ["mystical author", "cure for evil", "man who just ran 100 miles" ],
+
+    "Mile High" => ["man with anxiety", "women with glaucoma", "gutter punk with insomnia" ],
+
+    "Ben and Jerry's" => ["$6 ice cream cone", "trust fund stoner", "apathetic employee" ],
+
+    "Enchanted Ink" => ["anchor tattoo", "registered nurse", "lesbians getting matching rainbow tattoos" ],
+
+    "Boulder Cafe" => ["happiest hour", "rocky mountain oysters", "guy complaining about the loud music" ],
+
+    "Illegal Pete's" => ["pair programmers gazing dreamily into one another's eyes", "burrito as big as your head", "guy in a band with a gig coming up" ],
+
+    "Kasa" => ["fresh caught local walleye", "pure serenity", "umbrellas" ],
+
+    "Starbucks" => ["future gschool student", "milf", "prepubescent over-caffienated self-entitled teenagers"],
+
   }
 
