5ec8826893d827af2f3ea24cdb9df72506395552
Jenny Allar <JennyAllar@gmail.com>
2014-05-12 19:00:43 -0600

    Updated buildings.

---
+++ lib/buildings.rb
@@ -0,0 +1,22 @@
+BUILDINGS= [
+  {
+  0 => {:building1 => :quick_left,
+        :building2 => :yellow_deli,
+        :building3 => :send_grid},
+  1 => {:building1 => :galvanize,
+        :building2 => :ozo,
+        :building3 => :pasta_jays},
+  2 => {:building1 => :hapa_sushi,
+        :building2 => :boulder_book_store,
+        :building3 => :mile_high},
+  3 => {:building1 => :ben_and_jerrys,
+        :building2 => :enchanted_ink,
+        :building3 => :boulder_cafe},
+  4 => {:building1 => :fast_eddies,
+        :building2 => :lush,
+        :building3 => :lazy_dog},
+  5 => {:building1 => :illegal_petes,
+        :building2 => :kasa,
+        :building3 => :starbucks},
+}
+]
